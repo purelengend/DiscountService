d10b938a8749d232d13c30eae46b68e6490d805f
purelengend <nhathuy27301@gmail.com>
2023-01-16 13:24:10 +0700

    add exchange declare, optimal rabbitmq connection

--- Services/MessageProducer.cs
+++ Services/MessageProducer.cs
@@ -6,44 +6,43 @@ namespace DiscountAPI.Services;
 
 public class MessageProducer : IMessageProducer
 {
-    private readonly IConfiguration _config;
+  private readonly IConfiguration _config;
 
-    public MessageProducer(IConfiguration config)
+  public MessageProducer(IConfiguration config)
+  {
+    _config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", false, true)
+    .Build();
+  }
+  public void SendingMessage<T>(T message)
+  {
+    var factory = new ConnectionFactory()
     {
-        _config = new ConfigurationBuilder()
-        .AddJsonFile("appsettings.json", false, true)
-        .Build();
-    }
-    public void SendingMessage<T>(T message)
+      // HostName = "shark.rmq.cloudamqp.com",
+      // UserName = "etanosez",
+      // Password = "xMAGhBCpEIOUcEFPJjCAMYuHpOnX9pph",
+      // Port = 5672,
+      // VirtualHost = "etanosez"
+      HostName = _config.GetSection("RabbitMQConnectionStrings")["Hostname"],
+      UserName = _config.GetSection("RabbitMQConnectionStrings")["Username"],
+      Password = _config.GetSection("RabbitMQConnectionStrings")["Password"],
+      Port = int.Parse(_config.GetSection("RabbitMQConnectionStrings")["Port"]),
+      VirtualHost = _config.GetSection("RabbitMQConnectionStrings")["VirtualHost"],
+    };
+
+    using (var conn = factory.CreateConnection())
+    using (var channel = conn.CreateModel())
     {
-        var factory = new ConnectionFactory()
-        {
-            // HostName = "shark.rmq.cloudamqp.com",
-            // UserName = "etanosez",
-            // Password = "xMAGhBCpEIOUcEFPJjCAMYuHpOnX9pph",
-            // Port = 5672,
-            // VirtualHost = "etanosez"
-            HostName = _config.GetSection("RabbitMQConnectionStrings")["Hostname"],
-            UserName = _config.GetSection("RabbitMQConnectionStrings")["Username"],
-            Password = _config.GetSection("RabbitMQConnectionStrings")["Password"],
-            Port = int.Parse(_config.GetSection("RabbitMQConnectionStrings")["Port"]),
-            VirtualHost = _config.GetSection("RabbitMQConnectionStrings")["VirtualHost"],
-        };
+      channel.ExchangeDeclare(exchange: "CAPSTONE_EXCHANGE", type: ExchangeType.Fanout);
+      channel.QueueDeclare("discount", durable: true, exclusive: false);
 
-        var conn = factory.CreateConnection();
+      var jsonString = JsonSerializer.Serialize(message);
 
-        using var channel = conn.CreateModel();
+      var body = Encoding.UTF8.GetBytes(jsonString);
 
-        channel.QueueDeclare("discount", durable: true, exclusive: false, autoDelete: false);
+      channel.BasicPublish("CAPSTONE_EXCHANGE", "discount", body: body);
 
-        var jsonString = JsonSerializer.Serialize(message);
-
-        var body = Encoding.UTF8.GetBytes(jsonString);
-
-        Console.WriteLine(jsonString);
-
-        channel.BasicPublish("CAPSTONE_EXCHANGE", "discount", body: body);
-
-        Console.WriteLine("SendingMessage Done");
+      Console.WriteLine("SendingMessage Done");
     }
+  }
 }
